a0da1adfeb419d6d4fca90e059e1bbffcc80f1a2
Sujin Park <sujinpark1031@gmail.com>
2018-12-12 13:00:13 -0800

    Change database to postgresql for Heroku

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 5.2.2'
 gem 'jquery-rails', '~>4.3', '>= 4.3.3'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
+gem 'pg', '>= 0.18', '<2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_11_224229) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
