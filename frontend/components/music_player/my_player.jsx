775e3be45193b0ddcf8b6938e84475e6300160f4
Sujin Park <sujinpark1031@gmail.com>
2019-02-13 22:34:40 -0800

    Add comments to music player

--- frontend/components/music_player/my_player.jsx
+++ frontend/components/music_player/my_player.jsx
@@ -5,14 +5,17 @@ class MyPlayer extends React.Component {
 		super(props);
 		this.audio = new Audio(props.url);
 		this.audio.addEventListener("ended", props.onEnded);
+
+		//ensures the play button is disabled until song file is loaded
 		this.audio.addEventListener("loadeddata", props.onLoaded);
+
 		this.audio.addEventListener("timeupdate", () => {
 			const playedSeconds = this.audio.currentTime;
 			props.onProgress({
 				playedSeconds: playedSeconds
 			});
 		});
-		console.log(props);
+
 		props.playerRef(this.audio);
 		if (props.playing) {
 			this.audio.autoplay = true;
@@ -23,9 +26,13 @@ class MyPlayer extends React.Component {
 		this.audio.pause();
 	}
 
+	//use componentWillReceiveProps so that when I press pause, the song does not just reload on it's own and start at 0:00
 	componentWillReceiveProps(nextProps) {
+		//if we're onto a new song then change our audio's src to this new song's url
 		if (nextProps.url !== this.props.url) {
 			this.audio.src = nextProps.url;
+
+			//edge case: if i pause a song, then click next, the next song will load but won't start playing until I press play
 			if (nextProps.playing) {
 				this.audio.autoplay = true;
 			}
